e8c3e812de0aa48f5dd4bc8bf5e4cfedd8b75a6f
Olivier Schwab <olivier.schwab72@gmail.com>
2018-09-27 16:43:49 +0200

    made text search case insensitive

--- dcss_stats/core/utils.py
+++ dcss_stats/core/utils.py
@@ -5,17 +5,18 @@ import re
 
 def file_contains(filename, filter,regex=False):
     '''Determine whether a file contains a string.'''
-    if '20180419-135230' in filename:
+    if '20180920-115550' in filename:
         a=5
         b=2
 
 
     #filter=filter.encode('string-escape')
-
+    if not regex:
+        filter=filter.lower()
     with open(filename) as f:
         for num, line in zip(itertools.count(1), f.readlines()):
             if not regex:
-                if filter in line:
+                if filter in line.lower():
                     return filename, num, line
             else:
                 if re.search(filter, line):
